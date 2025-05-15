58498f2b3d9c3e2b3a485f72c44befed8633974e
be-camacho <be.camacho@duocuc.cl>
2025-05-15 19:09:52 -0400

    -cambios a serializer

--- authService/apiAuth/serializers.py
+++ authService/apiAuth/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer): # creo un serializer para rec
     username = serializers.CharField() #declaro que recibire un username
     password = serializers.CharField() #declaro que recibire un password
 
-    def validar(self, data): #hago una funcion para validar los datos
+    def validate(self, data): #hago una funcion para validar los datos
         username=data.get('username') 
         password=data.get('password')
         try: #intento obtener el usuario de la base de datos
@@ -16,12 +16,12 @@ class UserSerializer(serializers.ModelSerializer): # creo un serializer para rec
             raise serializers.ValidationError("Usuario o contraseña incorrectos")
         if not check_password(password, user.password): #se usa una if not para verificar si la contraseña no es correcta devido a que la contraseña se guarda en hash
             raise serializers.ValidationError("Usuario o contraseña incorrectos")
-        if user.rol.lower() == 'p': #verifico que el rol del usuario sea proveedor
-            return { #retorno el id del proveedor para su futuro uso y el rol del usuario
-                "idprov": user.idproveedor,
-                "rol": user.rol
-            }
-        else:
-            return { #retorno el el rol del usuario para su futuro uso
-                "rol": user.rol
-            }
+        
+        response_data = { #siempre retorno el rol
+            "rol": user.rol
+        }
+
+        if user.rol.lower() == 'p': #si el rol es proveedor añado el idproveedor al response_data
+            response_data["id"] = user.idproveedor 
+        
+        return response_data
