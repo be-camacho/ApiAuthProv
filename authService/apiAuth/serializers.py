c5ea76cce2c51f494bd97b9141a86c4462581add
be-camacho <be.camacho@duocuc.cl>
2025-05-15 18:19:55 -0400

    -se condiciona el retorno de datos segun el rol de el usuario -se documenta para un mejor manejo de errores a futuro

--- authService/apiAuth/serializers.py
+++ authService/apiAuth/serializers.py
@@ -1,23 +1,27 @@
 from rest_framework import serializers
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password # Importo la funcion check_password para verificar la contraseña con hash
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+class UserSerializer(serializers.ModelSerializer): # creo un serializer para recibir los datos a consultar en la base de datos
+    username = serializers.CharField() #declaro que recibire un username
+    password = serializers.CharField() #declaro que recibire un password
 
-    def validar(self, data):
-        username=data.get('username')
+    def validar(self, data): #hago una funcion para validar los datos
+        username=data.get('username') 
         password=data.get('password')
-
-        try:
+        try: #intento obtener el usuario de la base de datos
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             raise serializers.ValidationError("Usuario o contraseña incorrectos")
-        if not check_password(password, user.password):
+        if not check_password(password, user.password): #se usa una if not para verificar si la contraseña no es correcta devido a que la contraseña se guarda en hash
             raise serializers.ValidationError("Usuario o contraseña incorrectos")
-        return {
-            "idprov": user.idproveedor,
-            "rol": user.rol
-        }
+        if user.rol.lower() == 'p': #verifico que el rol del usuario sea proveedor
+            return { #retorno el id del proveedor para su futuro uso y el rol del usuario
+                "idprov": user.idproveedor,
+                "rol": user.rol
+            }
+        else:
+            return { #retorno el el rol del usuario para su futuro uso
+                "rol": user.rol
+            }
