99c69b36362b216441d5b22e6b8e29c9fc553ae2
be-camacho <be.camacho@duocuc.cl>
2025-05-15 19:48:14 -0400

    -se instalaron las dependencias de mysql y dotenv -se configuro la base de datos en el settings.py

--- authService/authService/settings.py
+++ authService/authService/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +80,16 @@ WSGI_APPLICATION = 'authService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),          # Nombre de tu base de datos
+        'USER': os.getenv('DB_USER'),          # Usuario de la base de datos
+        'PASSWORD': os.getenv('DB_PASSWORD'),  # Contraseña
+        'HOST': os.getenv('DB_HOST'),          # Endpoint de Aurora/RDS
+        'PORT': os.getenv('DB_PORT', '3306'),  # Puerto (default: 3306)
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
