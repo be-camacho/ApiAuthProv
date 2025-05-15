a8770e8fd7d2eb76abf0b78cfcf43651b83703f4
be-camacho <be.camacho@duocuc.cl>
2025-05-15 19:16:01 -0400

    -se creo la viewset .

--- authService/apiAuth/views.py
+++ authService/apiAuth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import UserSerializer as US
 
+class UserViewSet(viewsets.ModelViewSet):
+    @action(detail=False, methods=['post'], url_path='auth')
+    def auth(self, request):
+        serializer = US(data=request.data)
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=200)
+        
+        return Response(serializer.errors, status=400)
 # Create your views here.
