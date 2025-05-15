f47afaf377faa2f2f4ccba00d559e58254b01643
be-camacho <be.camacho@duocuc.cl>
2025-05-15 17:02:44 -0400

    -modelo de usuario creado

--- authService/apiAuth/models.py
+++ authService/apiAuth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    idproveedor = models.IntegerField()
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)  # Longitud para hash (recomendado)
+    rol = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'usuario'
+        managed = False
