e3fec87faa82bcb9fa9ed49bc6fde8c80123b483
be-camacho <be.camacho@duocuc.cl>
2025-05-15 20:09:29 -0400

    -cambios menores

--- authService/apiAuth/models.py
+++ authService/apiAuth/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=128) 
+    rol = models.CharField(max_length=1)
     idproveedor = models.IntegerField()
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=128)  # Longitud para hash (recomendado)
-    rol = models.CharField(max_length=20)
 
     class Meta:
-        db_table = 'usuario'
+        db_table = 'regprovs_user'
         managed = False
